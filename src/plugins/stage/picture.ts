6d841b3982138de5ab6296d26ff50f2915c33a43
moneyinto <woyaoqianjin@gmail.com>
2024-09-25 22:23:15 +0800

    缓存图片 提升渲染速度 优化体验

--- src/plugins/stage/picture.ts
+++ src/plugins/stage/picture.ts
@@ -19,6 +19,7 @@ export default class Picture {
     private _fill: Fill;
     private _outline: OutLine;
     private _actionAnimation: ActionAnimation;
+    private _cacheImage = new Map<string, Image>();
     constructor(
         stageConfig: StageConfig,
         canvas: Canvas,
@@ -36,71 +37,73 @@ export default class Picture {
     }
 
     public async draw(element: IPPTImageElement | IPPTLatexElement | IPPTChartElement) {
-        const cacheImage = await this._db.getFile(element.src) || defaultImage;
-        console.log("cacheImage");
-        if (cacheImage) {
-            const image = await new Promise<Image>((resolve) => {
+        let cacheImage = this._cacheImage.get(element.src);
+        if (!cacheImage) {
+            const imageFile = await this._db.getFile(element.src) || defaultImage;
+            cacheImage = await new Promise<Image>((resolve) => {
                 const image = new Image(this._canvas);
             
                 image.addEventListener("load", () => {
                     resolve(image);
                 });
 
-                image.src = cacheImage;
+                image.src = imageFile;
             })
-            console.log("image load", image);
-            const zoom = this._stageConfig.zoom;
-            const { x, y } = this._stageConfig.getStageOrigin();
 
-            this._ctx.save();
-
-            // 缩放画布
-            this._ctx.scale(zoom, zoom);
-
-            const ox = x + element.left + element.width / 2;
-            const oy = y + element.top + element.height / 2;
-
-            // 平移坐标原点
-            this._ctx.translate(ox, oy);
-            // 旋转画布
-            this._ctx.rotate((element.rotate / 180) * Math.PI);
-            // 水平垂直翻转
-            this._ctx.scale(element.flipH || 1, element.flipV || 1);
-
-            // 动画
-            this._actionAnimation.setElementStatus(element);
-
-            const path = getShapePath(SHAPE_TYPE.RECT, element.width, element.height) as Path2D;
-            if (element.fill) {
-                this._fill.draw(element.fill, path);
-            }
-
-            if (element.outline) {
-                this._outline.draw(element.outline, path);
-            }
-
-            if (element.shadow) {
-                this._shadow.draw(element.shadow, zoom);
-            }
-
-            // 设置透明度
-            this._ctx.globalAlpha = (100 - (element.opacity || 0)) / 100;
-
-            if (element.streach === 1) {
-                // 拉伸
-                this._ctx.drawImage(image, -element.width / 2, -element.height / 2, element.width, element.height);
-            } else {
-                // 缩放
-                let viewWidth = element.width;
-                let viewHeight = image.height / image.width * viewWidth;
-                if (viewHeight > element.height) {
-                    viewHeight = element.height;
-                    viewWidth = image.width / image.height * viewHeight;
-                }
-                this._ctx.drawImage(image, -viewWidth / 2, -viewHeight / 2, viewWidth, viewHeight);
-            }
-
-            this._ctx.restore();
+            this._cacheImage.set(element.src, cacheImage);
         }
+
+        const zoom = this._stageConfig.zoom;
+        const { x, y } = this._stageConfig.getStageOrigin();
+
+        this._ctx.save();
+
+        // 缩放画布
+        this._ctx.scale(zoom, zoom);
+
+        const ox = x + element.left + element.width / 2;
+        const oy = y + element.top + element.height / 2;
+
+        // 平移坐标原点
+        this._ctx.translate(ox, oy);
+        // 旋转画布
+        this._ctx.rotate((element.rotate / 180) * Math.PI);
+        // 水平垂直翻转
+        this._ctx.scale(element.flipH || 1, element.flipV || 1);
+
+        // 动画
+        this._actionAnimation.setElementStatus(element);
+
+        const path = getShapePath(SHAPE_TYPE.RECT, element.width, element.height) as Path2D;
+        if (element.fill) {
+            this._fill.draw(element.fill, path);
+        }
+
+        if (element.outline) {
+            this._outline.draw(element.outline, path);
+        }
+
+        if (element.shadow) {
+            this._shadow.draw(element.shadow, zoom);
+        }
+
+        // 设置透明度
+        this._ctx.globalAlpha = (100 - (element.opacity || 0)) / 100;
+
+        if (element.streach === 1) {
+            // 拉伸
+            this._ctx.drawImage(cacheImage, -element.width / 2, -element.height / 2, element.width, element.height);
+        } else {
+            // 缩放
+            let viewWidth = element.width;
+            let viewHeight = cacheImage.height / cacheImage.width * viewWidth;
+            if (viewHeight > element.height) {
+                viewHeight = element.height;
+                viewWidth = cacheImage.width / cacheImage.height * viewHeight;
+            }
+            this._ctx.drawImage(cacheImage, -viewWidth / 2, -viewHeight / 2, viewWidth, viewHeight);
+        }
+
+        this._ctx.restore();
     }
 }
